29a9db9504807a4e5a6ba9e3369a5e4e2a23d5dc
aksmith5239 <aklobby@gmail.com>
2020-08-25 15:24:41 -0600

    drag tasks to new list

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,55 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+//sortable jquery code
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  //event listeners:
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  }, 
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+    //loop over the current set of children in srotable list
+    $(this).children().each(function(){
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+//add task to temp array as an object
+      tempArr.push({
+        text: text, 
+        date: date
+      });
+    }); // children loop end
+    // trim down lists id to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();  
+    
+  }
+}); // end save tasks function
 // paragraph to text box logic
 $(".list-group").on("click", "p", function() {
     var text = $(this)
